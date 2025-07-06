b2ef1240923242efc84d7f2cf2c8dacfaea23392
AyushM198 <ayushmandawgade123@gmail.com>
2025-07-06 20:42:45 +0530

    Implemented video

--- src/app.jsx
+++ src/app.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import gsap from 'gsap'
 import {ScrollTrigger, SplitText} from 'gsap/all'
 gsap.registerPlugin(ScrollTrigger, SplitText)
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-purple-400 text-3xl underline'>Hello World</h1>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+      <div className='h-dvh bg-black'></div>
+    </main>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { navLinks } from '../../constants/index.js'
+import {useGSAP} from '@gsap/react';
+import gsap from 'gsap'
+
+const Navbar = () => {
+    useGSAP(() => {
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'nav',
+                start: 'bottom top',
+
+            }
+        });
+        navTween.from('nav',{backgroundColor: 'transparent'}, {backgroundColour: '#00000050',
+            backgroundFilter: 'blur(10px)',
+            duration: 1,
+            ease: 'power1.inOut',
+        })
+    }); 
+    return (
+        <nav>
+            <div>
+                <a href="#home" className='flex items-center gap-2'>
+                    <img src="/images/logo.png" alt="logo" className='w-10 h-10 object-contain' />
+                    <p>Velvet Pour</p>
+                </a>
+
+                <ul>
+                    {navLinks.map((link) => (
+                        <li key={link.id}>
+                            <a href={`#${link.id}`} className='text-gray-300 hover:text-purple-500'>
+                                {link.title}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
